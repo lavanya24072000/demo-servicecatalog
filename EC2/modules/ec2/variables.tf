f31f5b061542a27cb46db8c667d291dda8822e0a
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-06-05 23:46:20 +0530

    Create variables.tf

---
+++ EC2/modules/ec2/variables.tf
@@ -0,0 +1,19 @@
+
+variable "ami_id" {
+  description = "AMI ID for EC2"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "EC2 instance name"
+  type        = string
+}
+variable "subnet_id" {
+  description = "List of subnet IDs where EC2 instances will be deployed"
+  type        = string
+}
